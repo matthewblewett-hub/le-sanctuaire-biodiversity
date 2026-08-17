1a934d9b3ed7bc509fa77800483eafeef7f1285d
matthewblewett-hub <matthew.blewett@gmail.com>
2026-08-17 12:16:09 +0200

    Refresh branded UI immediately after deployment

--- sw.js
+++ sw.js
@@ -1,4 +1,31 @@
-const CACHE='le-sanctuaire-bio-v3';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/manifest.webmanifest','/apple-touch-icon.png','/icon-192.png','/icon-512.png']))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>hit)))});
+const CACHE='le-sanctuaire-bio-v4';
+const SHELL=['/index.html','/manifest.webmanifest','/apple-touch-icon.png','/icon-192.png','/icon-512.png'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),
+    self.clients.claim()
+  ]));
+});
+
+self.addEventListener('fetch',event=>{
+  const request=event.request;
+  if(request.mode==='navigate'){
+    event.respondWith(fetch(request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put('/index.html',copy));
+      return response;
+    }).catch(()=>caches.match('/index.html')));
+    return;
+  }
+  event.respondWith(caches.match(request).then(cached=>cached||fetch(request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(request,copy));
+    return response;
+  })));
+});
