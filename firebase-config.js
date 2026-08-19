5901d24a4314e4d8c63a06952d31adc1193c6baf
matthewblewett-hub <matthew.blewett@gmail.com>
2026-08-19 16:32:10 +0200

    Fix Firebase web configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAstIoTL_-AM4xeVbGkNE8VjoSJuSkN9as",
-  authDomain: "le-sanctuaire-biodiversity.firebaseapp.com",
-  projectId: "le-sanctuaire-biodiversity",
-  storageBucket: "le-sanctuaire-biodiversity.firebasestorage.app",
-  messagingSenderId: "226037868783",
-  appId: "1:226037868783:web:be5c4fe1cc9b0b2237931a"
+window.LE_SANCTUAIRE_FIREBASE = {
+  apiKey: "AIzaSyAstIoTL_-AM4xeVbGkNE8VjoSJuSkN9as",
+  authDomain: "le-sanctuaire-biodiversity.firebaseapp.com",
+  projectId: "le-sanctuaire-biodiversity",
+  storageBucket: "le-sanctuaire-biodiversity.firebasestorage.app",
+  messagingSenderId: "226037868783",
+  appId: "1:226037868783:web:be5c4fe1cc9b0b2237931a"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
